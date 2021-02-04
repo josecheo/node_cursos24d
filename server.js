4983339dbdcbd0487f36d2730f6ff25e4c74dcaa
joseche0 <joseche0.ja@gmail.com>
2021-02-04 15:09:27 -0500

    script

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.get("/", (req, res) => {
 
 // set port, listen for requests
 require("./app/routers/customer.routes")(app);
-app.listen(3000, () => {
+app.listen(4000, () => {
   console.log("Server is running on port 3000.");
 });
 
